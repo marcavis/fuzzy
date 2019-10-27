7827ad5703bbae26675fc0a478b5e024c4047cce
marcavis <marcavis@yahoo.com.br>
2019-10-27 20:09:35 -0300

    telinha

--- Fuzzy/src/fuzzy/Fuzzy.java
+++ Fuzzy/src/fuzzy/Fuzzy.java
@@ -3,6 +3,21 @@ package fuzzy;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
+import org.eclipse.swt.widgets.Menu;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.CoolBar;
+import org.eclipse.swt.widgets.CoolItem;
+
 public class Fuzzy {
 
 	protected Shell shell;
@@ -40,9 +55,41 @@ public class Fuzzy {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(800, 600);
 		shell.setText("SWT Application");
+		
+//		Menu menu = new Menu(shell, SWT.BAR);
+//		shell.setMenuBar(menu);
+//		
+//		MenuItem mntmNewItem_1 = new MenuItem(menu, SWT.NONE);
+//		mntmNewItem_1.setText("New Item");
+//		
+//		MenuItem mntmNewItem = new MenuItem(menu, SWT.NONE);
+//		mntmNewItem.setText("New Item");
+//		
+//		MenuItem menuItem = new MenuItem(menu, SWT.CASCADE);
+//		menuItem.setText("New SubMenu");
+//		
+//		Menu menu_1 = new Menu(menuItem);
+//		menuItem.setMenu(menu_1);
+		
+		Menu menu = new Menu(shell, SWT.BAR);
+		shell.setMenuBar(menu);
+		
+		MenuItem mntmarquivo = new MenuItem(menu, SWT.CASCADE);
+		mntmarquivo.setText("&Arquivo");
+		
+		Menu menu_1 = new Menu(mntmarquivo);
+		mntmarquivo.setMenu(menu_1);
+		
+		MenuItem mntmFechar= new MenuItem(menu_1, SWT.NONE);
+		mntmFechar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+			}
+		});
+		mntmFechar.setText("&Fechar");
 
 	}
-
 }
