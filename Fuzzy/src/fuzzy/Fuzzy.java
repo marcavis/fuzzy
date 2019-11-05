dc37329bddcebcc7cc252a463ca49a6fa48b3891
marcavis <marcavis@yahoo.com.br>
2019-11-04 23:48:23 -0300

    protótipo de mostrar gráfico e mostrar pertinência para 1º conjunto da variável destino

--- Fuzzy/src/fuzzy/Fuzzy.java
+++ Fuzzy/src/fuzzy/Fuzzy.java
@@ -1,38 +1,28 @@
 package fuzzy;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-
-import org.eclipse.swt.widgets.Menu;
-
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.CoolBar;
-import org.eclipse.swt.widgets.CoolItem;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.TabFolder;
-import org.eclipse.swt.widgets.TabItem;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.swt.custom.CTabFolder;
-import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.layout.RowLayout;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class Fuzzy {
 
@@ -43,14 +33,10 @@ public class Fuzzy {
 //	private Text text_3;
 //	private Text text_4;
 //	private Text text_5;
-	private Text textSup1;
-	private Text textNuc1;
-	private Text textSup2;
-	private Text textNuc2;
-	private Text textSup3;
-	private Text textNuc3;
+	
 	private Text text_nomeVarDestino;
 	private ArrayList<Text[]> variaveis;
+	private Text[] varDestino;
 
 	/**
 	 * Launch the application.
@@ -133,6 +119,8 @@ public class Fuzzy {
 		
 		criarAba(tabFolder, variaveis);
 		
+		varDestino = new Text[12];
+		
 		Composite composite_2 = new Composite(composite, SWT.NONE);
 		composite_2.setBounds(485, 28, 475, 514);
 		
@@ -140,42 +128,60 @@ public class Fuzzy {
 		group_1.setBounds(10, 46, 443, 177);
 		
 		Label label = new Label(group_1, SWT.BORDER);
-		label.setText("Supaa");
-		label.setBounds(10, 42, 40, 20);
+		label.setText("Suporte");
+		label.setBounds(10, 42, 80, 20);
 		
 		Label label_1 = new Label(group_1, SWT.BORDER);
-		label_1.setText("Nuc");
-		label_1.setBounds(10, 84, 40, 20);
+		label_1.setText("Núcleo");
+		label_1.setBounds(10, 84, 80, 20);
 		
-		textSup1 = new Text(group_1, SWT.BORDER);
-		textSup1.setBounds(56, 42, 78, 26);
+		varDestino[0] = new Text(group_1, SWT.BORDER);
+		varDestino[0].setBounds(96, 42, 42, 26);
 		
-		textNuc1 = new Text(group_1, SWT.BORDER);
-		textNuc1.setBounds(56, 82, 78, 26);
+		varDestino[1] = new Text(group_1, SWT.BORDER);
+		varDestino[1].setBounds(146, 42, 42, 26);
+		
+		varDestino[2] = new Text(group_1, SWT.BORDER);
+		varDestino[2].setBounds(96, 82, 42, 26);
+		
+		varDestino[3] = new Text(group_1, SWT.BORDER);
+		varDestino[3].setBounds(146, 82, 42, 26);
 		
 		Label label_2 = new Label(group_1, SWT.BORDER);
 		label_2.setText("Pouco");
-		label_2.setBounds(67, 16, 57, 20);
+		label_2.setBounds(120, 16, 50, 20);
 		
 		Label label_3 = new Label(group_1, SWT.BORDER);
 		label_3.setText("M\u00E9dio");
-		label_3.setBounds(174, 16, 54, 20);
+		label_3.setBounds(240, 16, 50, 20);
 		
-		textSup2 = new Text(group_1, SWT.BORDER);
-		textSup2.setBounds(161, 42, 78, 26);
+		varDestino[4] = new Text(group_1, SWT.BORDER);
+		varDestino[4].setBounds(216, 42, 42, 26);
 		
-		textNuc2 = new Text(group_1, SWT.BORDER);
-		textNuc2.setBounds(161, 78, 78, 26);
+		varDestino[5] = new Text(group_1, SWT.BORDER);
+		varDestino[5].setBounds(266, 42, 42, 26);
+		
+		varDestino[6] = new Text(group_1, SWT.BORDER);
+		varDestino[6].setBounds(216, 82, 42, 26);
+		
+		varDestino[7] = new Text(group_1, SWT.BORDER);
+		varDestino[7].setBounds(266, 82, 42, 26);
 		
 		Label label_4 = new Label(group_1, SWT.BORDER);
 		label_4.setText("Muito");
-		label_4.setBounds(279, 16, 54, 20);
+		label_4.setBounds(360, 16, 50, 20);
 		
-		textSup3 = new Text(group_1, SWT.BORDER);
-		textSup3.setBounds(263, 42, 78, 26);
+		varDestino[8] = new Text(group_1, SWT.BORDER);
+		varDestino[8].setBounds(336, 42, 42, 26);
 		
-		textNuc3 = new Text(group_1, SWT.BORDER);
-		textNuc3.setBounds(263, 78, 78, 26);
+		varDestino[9] = new Text(group_1, SWT.BORDER);
+		varDestino[9].setBounds(386, 42, 42, 26);
+		
+		varDestino[10] = new Text(group_1, SWT.BORDER);
+		varDestino[10].setBounds(336, 82, 42, 26);
+		
+		varDestino[11] = new Text(group_1, SWT.BORDER);
+		varDestino[11].setBounds(386, 82, 42, 26);
 		
 		Label lblNewLabel_1 = new Label(composite_2, SWT.BORDER);
 		lblNewLabel_1.setAlignment(SWT.CENTER);
@@ -186,12 +192,29 @@ public class Fuzzy {
 		text_nomeVarDestino.setBounds(214, 18, 220, 26);
 		text_nomeVarDestino.setText("VarDestino");
 		
-		Label label_5 = new Label(composite_2, SWT.BORDER);
-		label_5.setBounds(10, 247, 443, 261);
+		CLabel label_5 = new CLabel(composite_2, SWT.BORDER);
+		label_5.setBounds(10, 240, 400, 240);
 		
-		Button btnNewButton = new Button(composite, SWT.NONE);
-		btnNewButton.setBounds(633, 548, 161, 47);
-		btnNewButton.setText("Executar");
+		Button btnVis1 = new Button(group_1, SWT.NONE);
+		btnVis1.setBounds(160, 120, 161, 47);
+		btnVis1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try {
+					BufferedImage imgGrafVarDestino = geraGrafico(varDestino);
+					ImageIO.write(imgGrafVarDestino, "bmp", new File("temp/_graficovd.bmp"));
+					Image graficoVarDestino = new Image(null, "temp/_graficovd.bmp");
+					carregaImagem(label_5, graficoVarDestino);
+				} catch (Exception f) {
+					f.printStackTrace();
+				}
+			}
+		});
+		btnVis1.setText("Visualizar");
+		
+		Button btnExecutar = new Button(composite, SWT.NONE);
+		btnExecutar.setBounds(633, 548, 161, 47);
+		btnExecutar.setText("Executar");
 
 	}
 	
@@ -208,44 +231,110 @@ public class Fuzzy {
 		group.setBounds(10, 10, 443, 177);
 		
 		Label lblSup = new Label(group, SWT.BORDER);
-		lblSup.setBounds(10, 42, 40, 20);
+		lblSup.setBounds(10, 42, 80, 20);
 		lblSup.setText("Suporte");
 		
 		Label lblNuc = new Label(group, SWT.BORDER);
 		lblNuc.setText("Núcleo");
-		lblNuc.setBounds(10, 84, 40, 20);
+		lblNuc.setBounds(10, 84, 80, 20);
 		
 		estaVariavel[0] = new Text(group, SWT.BORDER);
-		estaVariavel[0].setBounds(56, 42, 78, 26);
+		estaVariavel[0].setBounds(96, 42, 42, 26);
 		
 		estaVariavel[1] = new Text(group, SWT.BORDER);
-		estaVariavel[1].setBounds(56, 82, 78, 26);
+		estaVariavel[1].setBounds(146, 42, 42, 26);
+		
+		estaVariavel[2] = new Text(group, SWT.BORDER);
+		estaVariavel[2].setBounds(96, 82, 42, 26);
+		
+		estaVariavel[3] = new Text(group, SWT.BORDER);
+		estaVariavel[3].setBounds(146, 82, 42, 26);
 		
 		Label lblPouco = new Label(group, SWT.BORDER);
 		lblPouco.setText("Pouco");
-		lblPouco.setBounds(67, 16, 57, 20);
+		lblPouco.setBounds(120, 16, 50, 20);
+		lblPouco.setAlignment(SWT.CENTER);
 		
 		Label lblMdio = new Label(group, SWT.BORDER);
 		lblMdio.setText("M\u00E9dio");
-		lblMdio.setBounds(174, 16, 54, 20);
+		lblMdio.setBounds(240, 16, 50, 20);
+		lblMdio.setAlignment(SWT.CENTER);
 		
-		estaVariavel[2] = new Text(group, SWT.BORDER);
-		estaVariavel[2].setBounds(161, 42, 78, 26);
+		estaVariavel[4] = new Text(group, SWT.BORDER);
+		estaVariavel[4].setBounds(216, 42, 42, 26);
 		
-		estaVariavel[3] = new Text(group, SWT.BORDER);
-		estaVariavel[3].setBounds(161, 78, 78, 26);
+		estaVariavel[5] = new Text(group, SWT.BORDER);
+		estaVariavel[5].setBounds(266, 42, 42, 26);
+		
+		estaVariavel[6] = new Text(group, SWT.BORDER);
+		estaVariavel[6].setBounds(216, 82, 42, 26);
+		
+		estaVariavel[7] = new Text(group, SWT.BORDER);
+		estaVariavel[7].setBounds(266, 82, 42, 26);
 		
 		Label lblMuito = new Label(group, SWT.BORDER);
 		lblMuito.setText("Muito");
-		lblMuito.setBounds(279, 16, 54, 20);
+		lblMuito.setBounds(360, 16, 50, 20);
+		lblMuito.setAlignment(SWT.CENTER);
 		
-		estaVariavel[4] = new Text(group, SWT.BORDER);
-		estaVariavel[4].setBounds(263, 42, 78, 26);
+		estaVariavel[8] = new Text(group, SWT.BORDER);
+		estaVariavel[8].setBounds(336, 42, 42, 26);
 		
-		estaVariavel[5] = new Text(group, SWT.BORDER);
-		estaVariavel[5].setBounds(263, 78, 78, 26);
+		estaVariavel[9] = new Text(group, SWT.BORDER);
+		estaVariavel[9].setBounds(386, 42, 42, 26);
 		
-		Label lblNewLabel = new Label(composite_3, SWT.BORDER);
-		lblNewLabel.setBounds(10, 209, 443, 261);
+		estaVariavel[10] = new Text(group, SWT.BORDER);
+		estaVariavel[10].setBounds(336, 82, 42, 26);
+		
+		estaVariavel[11] = new Text(group, SWT.BORDER);
+		estaVariavel[11].setBounds(386, 82, 42, 26);
+		
+		CLabel lblNewLabel = new CLabel(composite_3, SWT.BORDER);
+		lblNewLabel.setBounds(10, 209, 400, 240);
+	}
+	
+	private BufferedImage geraGrafico(Text[] dados) {
+		BufferedImage res = new BufferedImage(400, 240, BufferedImage.TYPE_INT_RGB);
+		WritableRaster raster = res.getRaster();
+		
+		double[] valores = new double[dados.length];
+		for (int i = 0; i < dados.length; i++) {
+			String str = dados[i].getText();
+			//System.out.println(text.getText() + "?");
+			try	{
+				valores[i] = Double.parseDouble(str);
+			}
+			catch(NumberFormatException e) {
+				valores[i] = 0.0;
+			}
+		}
+		
+		Conjunto c = new Conjunto("Teste", valores[0], valores[1], valores[2], valores[3]);
+		for(int i = 0; i < 100; i++) {
+			System.out.println("Pertinência para " + (i) + ": " + c.pertinencia(i));
+		}
+		
+		for(int i = 0; i < 200; i++) {
+			for(int j = 0; j < 200; j++) {
+				raster.setPixel(10 + i, j, new int[] {i, j, 1});
+			}
+		}
+		
+		try {
+			res.setData(raster);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+	
+	private void carregaImagem(CLabel label, Image img) {
+		label.setBackground(img);
+		label.setBounds(
+				label.getBounds().x,
+				label.getBounds().y,
+				img.getImageData().width,
+				img.getImageData().height
+				);
 	}
 }
