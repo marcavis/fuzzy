e3223960b4cc3c7fbaa979028997f688e54f241b
marcavis <marcavis@yahoo.com.br>
2019-11-11 02:27:42 -0300

    use spinners e tal

--- Fuzzy/src/fuzzy/Fuzzy.java
+++ Fuzzy/src/fuzzy/Fuzzy.java
@@ -13,11 +13,17 @@ import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabFolder2Adapter;
 import org.eclipse.swt.custom.CTabFolderEvent;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
@@ -26,10 +32,12 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Text;
 
 public class Fuzzy {
 
+	protected Display display;
 	protected Shell shell;
 //	private Text text_1;
 //	private Text text_2;
@@ -37,9 +45,11 @@ public class Fuzzy {
 //	private Text text_4;
 //	private Text text_5;
 	
-	private Text text_nomeVarDestino;
 	private ArrayList<Text[]> variaveis;
-	private Text[] varDestino;
+
+	private CTabFolder tabFolder;
+
+	private Combo varDestino;
 
 	/**
 	 * Launch the application.
@@ -74,8 +84,11 @@ public class Fuzzy {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(978, 677);
+		//shell.setSize(978, 677);
 		shell.setText("SWT Application");
+		GridLayout mainGrid = new GridLayout();
+		mainGrid.numColumns = 3;
+		shell.setLayout(mainGrid);
 		
 //		Menu menu = new Menu(shell, SWT.BAR);
 //		shell.setMenuBar(menu);
@@ -101,7 +114,7 @@ public class Fuzzy {
 		Menu menu_1 = new Menu(mntmarquivo);
 		mntmarquivo.setMenu(menu_1);
 		
-		MenuItem mntmFechar= new MenuItem(menu_1, SWT.NONE);
+		MenuItem mntmFechar = new MenuItem(menu_1, SWT.NONE);
 		mntmFechar.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -110,109 +123,43 @@ public class Fuzzy {
 		});
 		mntmFechar.setText("&Fechar");
 		
-		Composite composite = new Composite(shell, SWT.NONE);
-		composite.setBounds(0, 0, 960, 605);
+		Button btnNovaVar = new Button(shell, SWT.NONE);
+		btnNovaVar.setText("Nova Variável");
 		
-		Composite composite_1 = new Composite(composite, SWT.NONE);
-		composite_1.setBounds(0, 28, 479, 577);
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
 		
-		CTabFolder tabFolder = new CTabFolder(composite_1, SWT.BORDER);
-		tabFolder.setBounds(10, 10, 469, 567);
+		tabFolder = new CTabFolder(shell, SWT.BORDER);
 		tabFolder.setSelectionBackground(Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT));
+		tabFolder.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
 		
 		tabFolder.addCTabFolder2Listener(new CTabFolder2Adapter() {
-		      public void close(CTabFolderEvent event) {
-		        System.out.println("want to close");
-		        MessageBox dialog = new MessageBox(shell, SWT.ICON_QUESTION | SWT.OK| SWT.CANCEL);
-		        dialog.setText("Confirmação");
-		        System.out.println(event.item);
-		        CTabItem text = (CTabItem) event.item;
-		        dialog.setMessage("Deseja realmente excluir a variável " + text.getText() + "?");
-		        int retorno = dialog.open();
-		        event.doit = retorno == 32;
-//		        if (event.item.equals(specialItem)) {
-//		          event.doit = false;
-//		        }
-		      }
-		    });
+			public void close(CTabFolderEvent event) {
+				System.out.println("want to close");
+				MessageBox dialog = new MessageBox(shell, SWT.ICON_QUESTION | SWT.OK| SWT.CANCEL);
+				dialog.setText("Confirmação");
+				System.out.println(event.item);
+				CTabItem text = (CTabItem) event.item;
+				dialog.setMessage("Deseja realmente excluir a variável " + text.getText() + "?");
+				int retorno = dialog.open();
+				event.doit = retorno == 32; //confirmou exclusão
+				if(event.doit) {
+					atualizarComboVarDestino();
+				}
+			}
+		});
+		
+		btnNovaVar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(tabFolder.getItemCount() < 8) {
+					criarAba(tabFolder, variaveis);
+					atualizarComboVarDestino();
+				}
+			}
+		});
 		
 		criarAba(tabFolder, variaveis);
-		
-		varDestino = new Text[12];
-		
-//		Composite composite_2 = new Composite(composite, SWT.NONE);
-//		composite_2.setBounds(485, 28, 475, 514);
-//		
-//		Group group_1 = new Group(composite_2, SWT.NONE);
-//		group_1.setBounds(10, 46, 443, 177);
-//		
-//		Label label = new Label(group_1, SWT.BORDER);
-//		label.setText("Suporte");
-//		label.setBounds(10, 42, 80, 20);
-//		
-//		Label label_1 = new Label(group_1, SWT.BORDER);
-//		label_1.setText("Núcleo");
-//		label_1.setBounds(10, 84, 80, 20);
-//		
-//		varDestino[0] = new Text(group_1, SWT.BORDER);
-//		varDestino[0].setBounds(96, 42, 42, 26);
-//		
-//		varDestino[1] = new Text(group_1, SWT.BORDER);
-//		varDestino[1].setBounds(146, 42, 42, 26);
-//		
-//		varDestino[2] = new Text(group_1, SWT.BORDER);
-//		varDestino[2].setBounds(96, 82, 42, 26);
-//		
-//		varDestino[3] = new Text(group_1, SWT.BORDER);
-//		varDestino[3].setBounds(146, 82, 42, 26);
-//		
-//		Label label_2 = new Label(group_1, SWT.BORDER);
-//		label_2.setText("Pouco");
-//		label_2.setBounds(120, 16, 50, 20);
-//		
-//		Label label_3 = new Label(group_1, SWT.BORDER);
-//		label_3.setText("M\u00E9dio");
-//		label_3.setBounds(240, 16, 50, 20);
-//		
-//		varDestino[4] = new Text(group_1, SWT.BORDER);
-//		varDestino[4].setBounds(216, 42, 42, 26);
-//		
-//		varDestino[5] = new Text(group_1, SWT.BORDER);
-//		varDestino[5].setBounds(266, 42, 42, 26);
-//		
-//		varDestino[6] = new Text(group_1, SWT.BORDER);
-//		varDestino[6].setBounds(216, 82, 42, 26);
-//		
-//		varDestino[7] = new Text(group_1, SWT.BORDER);
-//		varDestino[7].setBounds(266, 82, 42, 26);
-//		
-//		Label label_4 = new Label(group_1, SWT.BORDER);
-//		label_4.setText("Muito");
-//		label_4.setBounds(360, 16, 50, 20);
-//		
-//		varDestino[8] = new Text(group_1, SWT.BORDER);
-//		varDestino[8].setBounds(336, 42, 42, 26);
-//		
-//		varDestino[9] = new Text(group_1, SWT.BORDER);
-//		varDestino[9].setBounds(386, 42, 42, 26);
-//		
-//		varDestino[10] = new Text(group_1, SWT.BORDER);
-//		varDestino[10].setBounds(336, 82, 42, 26);
-//		
-//		varDestino[11] = new Text(group_1, SWT.BORDER);
-//		varDestino[11].setBounds(386, 82, 42, 26);
-//		
-//		Label lblNewLabel_1 = new Label(composite_2, SWT.BORDER);
-//		lblNewLabel_1.setAlignment(SWT.CENTER);
-//		lblNewLabel_1.setBounds(10, 18, 180, 22);
-//		lblNewLabel_1.setText("Vari\u00E1vel de Destino:");
-//		
-//		text_nomeVarDestino = new Text(composite_2, SWT.BORDER);
-//		text_nomeVarDestino.setBounds(214, 18, 220, 26);
-//		text_nomeVarDestino.setText("VarDestino");
-//		
-//		CLabel label_5 = new CLabel(composite_2, SWT.BORDER);
-//		label_5.setBounds(10, 240, 400, 240);
 		
 //		Button btnVis1 = new Button(group_1, SWT.NONE);
 //		btnVis1.setBounds(160, 120, 161, 47);
@@ -231,91 +178,156 @@ public class Fuzzy {
 //		});
 //		btnVis1.setText("Visualizar");
 		
-		Button btnExecutar = new Button(composite, SWT.NONE);
-		btnExecutar.setBounds(633, 548, 161, 47);
+		Composite compo = new Composite(shell, SWT.NONE);
+		GridLayout layoutCompo2 = new GridLayout();
+	    layoutCompo2.numColumns = 2;
+		compo.setLayout(layoutCompo2);
+	    
+		Label temp = new Label(compo, SWT.NONE);
+		temp.setText("Variável Destino");
+		
+		
+		varDestino = new Combo(compo, SWT.READ_ONLY);
+		atualizarComboVarDestino();
+		
+		Button btnExecutar = new Button(shell, SWT.NONE);
+		//btnExecutar.setBounds(633, 548, 161, 47);
 		btnExecutar.setText("Executar");
 
 	}
 	
+	private void atualizarComboVarDestino() {
+		String[] nomesVars = new String[tabFolder.getItemCount()];
+		for (int i = 0; i < tabFolder.getItemCount(); i++) {
+			nomesVars[i] = tabFolder.getItem(i).getText();
+		}
+		varDestino.setItems(nomesVars);
+	}
+
 	public void criarAba(CTabFolder tabFolder, ArrayList<Text[]> variaveis) {
-		Text[] estaVariavel = new Text[12];
-		
+		Spinner[] estaVariavel = new Spinner[12];
+
 		GridLayout layout = new GridLayout();
-	    layout.numColumns = 3;
-	    
+	    layout.numColumns = 7;
 		
 		CTabItem tbtmNewItem = new CTabItem(tabFolder, SWT.CLOSE);
-		tbtmNewItem.setText("Var1");
+		tbtmNewItem.setText("Var" + (tabFolder.getItemCount()));
 		
 		Composite thisComposite = new Composite(tabFolder, SWT.NONE);
 		tbtmNewItem.setControl(thisComposite);
+		thisComposite.setLayout(layout);
+		thisComposite.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
 		
-		Group group = new Group(thisComposite, SWT.NONE);
-		group.setBounds(10, 10, 443, 177);
+		GridData layoutBtnDup = new GridData();
+		layoutBtnDup.horizontalAlignment = GridData.FILL;
+		layoutBtnDup.horizontalSpan = 2;
 		
-		Label lblSup = new Label(group, SWT.BORDER);
-		lblSup.setBounds(10, 42, 80, 20);
-		lblSup.setText("Suporte");
+		Label lblNomeVar = new Label(thisComposite, SWT.NONE);
+		lblNomeVar.setText("Nome da Variável");
+		lblNomeVar.setLayoutData(layoutBtnDup);
 		
-		Label lblNuc = new Label(group, SWT.BORDER);
-		lblNuc.setText("Núcleo");
-		lblNuc.setBounds(10, 84, 80, 20);
+		Text textNomeVar = new Text(thisComposite, SWT.BORDER);
+		textNomeVar.setText(tbtmNewItem.getText());
+		textNomeVar.setLayoutData(layoutBtnDup);
+		textNomeVar.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent arg0) {
+				Text p = (Text) arg0.getSource();
+				if(p.getText().length() > 0) {
+					tbtmNewItem.setText(p.getText());
+				} else {
+					tbtmNewItem.setText("Vazio");
+				}
+				atualizarComboVarDestino();
+			}
+		});
+		Label nada = new Label(thisComposite, SWT.NONE);
+		new Label(thisComposite, SWT.NONE);
+		new Label(thisComposite, SWT.NONE);
 		
-		estaVariavel[0] = new Text(group, SWT.BORDER);
-		estaVariavel[0].setBounds(96, 42, 42, 26);
+		Label lblUniverso = new Label(thisComposite, SWT.NONE);
+		lblUniverso.setText("Valores Possíveis");
+		lblUniverso.setLayoutData(layoutBtnDup);
 		
-		estaVariavel[1] = new Text(group, SWT.BORDER);
-		estaVariavel[1].setBounds(146, 42, 42, 26);
+		Spinner univMin = new Spinner(thisComposite, SWT.NONE);
 		
-		estaVariavel[2] = new Text(group, SWT.BORDER);
-		estaVariavel[2].setBounds(96, 82, 42, 26);
+		univMin.setLayoutData(layoutBtnDup);
 		
-		estaVariavel[3] = new Text(group, SWT.BORDER);
-		estaVariavel[3].setBounds(146, 82, 42, 26);
+		Spinner univMax = new Spinner(thisComposite, SWT.NONE);
+		univMax.setLayoutData(layoutBtnDup);
+		configuraSpinner(univMin);
+		configuraSpinner(univMax);
+		univMax.setSelection(10000);
 		
-		Label lblPouco = new Label(group, SWT.BORDER);
+		new Label(thisComposite, SWT.NONE);
+		new Label(thisComposite, SWT.NONE);
+		
+		Text lblPouco = new Text(thisComposite, SWT.BORDER);
 		lblPouco.setText("Pouco");
-		lblPouco.setBounds(120, 16, 50, 20);
-		lblPouco.setAlignment(SWT.CENTER);
+		lblPouco.setLayoutData(layoutBtnDup);
 		
-		Label lblMdio = new Label(group, SWT.BORDER);
+		Text lblMdio = new Text(thisComposite, SWT.BORDER);
 		lblMdio.setText("M\u00E9dio");
-		lblMdio.setBounds(240, 16, 50, 20);
-		lblMdio.setAlignment(SWT.CENTER);
+		lblMdio.setLayoutData(layoutBtnDup);
 		
-		estaVariavel[4] = new Text(group, SWT.BORDER);
-		estaVariavel[4].setBounds(216, 42, 42, 26);
-		
-		estaVariavel[5] = new Text(group, SWT.BORDER);
-		estaVariavel[5].setBounds(266, 42, 42, 26);
-		
-		estaVariavel[6] = new Text(group, SWT.BORDER);
-		estaVariavel[6].setBounds(216, 82, 42, 26);
-		
-		estaVariavel[7] = new Text(group, SWT.BORDER);
-		estaVariavel[7].setBounds(266, 82, 42, 26);
-		
-		Label lblMuito = new Label(group, SWT.BORDER);
+		Text lblMuito = new Text(thisComposite, SWT.BORDER);
 		lblMuito.setText("Muito");
-		lblMuito.setBounds(360, 16, 50, 20);
-		lblMuito.setAlignment(SWT.CENTER);
+		lblMuito.setLayoutData(layoutBtnDup);
 		
-		estaVariavel[8] = new Text(group, SWT.BORDER);
-		estaVariavel[8].setBounds(336, 42, 42, 26);
+		GridData layout5Esp = new GridData();
+		layout5Esp.horizontalAlignment = GridData.FILL;
+		layout5Esp.horizontalSpan = 5;
+		nada = new Label(thisComposite, SWT.NONE);
+		nada.setLayoutData(layout5Esp);
 		
-		estaVariavel[9] = new Text(group, SWT.BORDER);
-		estaVariavel[9].setBounds(386, 42, 42, 26);
+		Button btnHabilitar = new Button(thisComposite, SWT.CHECK);
+		btnHabilitar.setLayoutData(layoutBtnDup);
+		btnHabilitar.setSelection(true);
+		btnHabilitar.setText("Habilitar");
+		btnHabilitar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println();
+				int[] conjunto3 = new int[] {4, 5, 10, 11};
+				for (int i : conjunto3) {
+					estaVariavel[i].setVisible(btnHabilitar.getSelection());
+				}
+			}
+		});
 		
-		estaVariavel[10] = new Text(group, SWT.BORDER);
-		estaVariavel[10].setBounds(336, 82, 42, 26);
+		Label lblSup = new Label(thisComposite, SWT.BORDER);
+		lblSup.setText("Suporte");
+		lblSup.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
 		
-		estaVariavel[11] = new Text(group, SWT.BORDER);
-		estaVariavel[11].setBounds(386, 82, 42, 26);
-		
+		for (int i = 0; i < estaVariavel.length; i++) {
+			if(i == 6) {
+				Label lblNuc = new Label(thisComposite, SWT.BORDER);
+				lblNuc.setText("Núcleo");
+				lblNuc.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
+			}
+			estaVariavel[i] = new Spinner(thisComposite, SWT.NONE);
+			configuraSpinner(estaVariavel[i]);
+		}		
 		CLabel lblNewLabel = new CLabel(thisComposite, SWT.BORDER);
-		lblNewLabel.setBounds(10, 209, 400, 240);
+		GridData imagemLayout = new GridData(); 
+		imagemLayout.widthHint = 400;
+		imagemLayout.heightHint = 300;
+		imagemLayout.horizontalSpan = 7;
+		lblNewLabel.setLayoutData(imagemLayout);
+		
 	}
 	
+	private void configuraSpinner(Spinner s) {
+//		FontData[] fD = s.getFont().getFontData();
+//		fD[0].setHeight(7);
+//		s.setFont( new Font(display,fD[0]));
+		s.setSelection(0);
+		s.setDigits(2);
+		s.setMinimum(-100000);
+		s.setMaximum(100000);
+		s.setIncrement(500);
+	}
+
 	private BufferedImage geraGrafico(Text[] dados) {
 		BufferedImage res = new BufferedImage(400, 240, BufferedImage.TYPE_INT_RGB);
 		WritableRaster raster = res.getRaster();
